59125da68fc705698b65f6ad90a600d5009bba1f
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-05-28 12:05:41 -0400

    Added functionality to play next song in script file

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
+// Audio control buttons
 const playButton = document.querySelector(".fa-play");
+const nextButton = document.querySelector(".fa-forward");
+const prevButton = document.querySelector(".fa-backward");
+
+// Song image and audio
 const audio = document.getElementById("audio");
 const image = document.getElementById("image");
 
 let playing = false;
-let songs = 2;
+const songs = [
+  {
+    id: 1,
+    name: "Gurenge",
+    artist: "LISA",
+  },
+  {
+    id: 2,
+    name: "Unravel",
+    artist: "Ling Toshite Sigure",
+  },
+];
+let currentSong = 1;
 
-function replace(element, toReplace, replaceWith) {
+const replaceIcon = (element, toReplace, replaceWith) => {
   element.classList.remove(`fa-${toReplace}`);
   element.classList.add(`fa-${replaceWith}`);
-}
+};
+
+const playNext = () => {
+  console.log("Next");
+  if (currentSong === songs.length) {
+    console.log(currentSong);
+    currentSong = songs[0].id;
+  } else {
+    console.log(currentSong);
+    currentSong++;
+  }
+
+  audio.setAttribute("src", `/music/song-${currentSong}.mp3`);
+  image.setAttribute("src", `/images/song-${currentSong}.jpeg`);
+
+  console.log(audio);
+  console.log(image);
+};
 
 playButton.addEventListener("click", () => {
   if (playing) {
     audio.pause();
-    replace(playButton, "pause", "play");
+    replaceIcon(playButton, "pause", "play");
     playing = false;
   } else {
     audio.play();
-    replace(playButton, "play", "pause");
+    replaceIcon(playButton, "play", "pause");
     playing = true;
   }
+});
+
+nextButton.addEventListener("click", () => {
+  console.log("Click");
+  playing = false;
+  audio.pause();
+  playNext();
+  playing = true;
+  audio.play();
 });
